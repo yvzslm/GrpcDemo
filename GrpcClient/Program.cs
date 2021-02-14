7b3b0266698a734a627a650eadb976afc2e46690
yavuz <yavuz.altun09@gmail.com>
2021-02-14 23:34:16 +0300

    initial commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,92 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+using static GrpcServer.GreetingService;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new GreetingServiceClient(channel);
+            var greeting = new Greeting()
+            {
+                FirstName = "Yavuz",
+                LastName = "Altun"
+            };
+
+            await DoGreet(client, greeting);
+            await DoGreetManyTimes(client, greeting);
+            await DoLongGreet(client, greeting);
+            await DoGreetEveryone(client, greeting);
+
+            Console.Read();
+        }
+
+        static async Task DoGreet(GreetingServiceClient client, Greeting greeting)
+        {
+            var response = await client.GreetAsync(new GreetingRequest() { Greeting = greeting });
+            Console.WriteLine(response.Result);
+        }
+
+        static async Task DoGreetManyTimes(GreetingServiceClient client, Greeting greeting)
+        {
+            var stream = client.GreetManyTimes(new GreetManyTimesRequest() { Greeting = greeting });
+            while (await stream.ResponseStream.MoveNext())
+            {
+                Console.WriteLine("Client receiving:");
+                Console.WriteLine(stream.ResponseStream.Current.Result);
+            }
+
+            //await foreach (var item in stream.ResponseStream.ReadAllAsync())
+            //{
+            //    Console.WriteLine(item.Result);
+            //}
+        }
+
+        static async Task DoLongGreet(GreetingServiceClient client, Greeting greeting)
+        {
+            var stream = client.LongGreet();
+            Console.WriteLine("Client sending:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Greeting {i}: {greeting}");
+                await stream.RequestStream.WriteAsync(new LongGreetRequest() { Greeting = greeting });
+                await Task.Delay(1000);
+            }
+
+            await stream.RequestStream.CompleteAsync();
+            var response = await stream.ResponseAsync;
+
+            Console.WriteLine("Client received:");
+            Console.WriteLine(response.Result);
+        }
+
+        static async Task DoGreetEveryone(GreetingServiceClient client, Greeting greeting)
+        {
+            var stream = client.GreetEveryone();
+            var readerTask = Task.Run(async () =>
+            {
+                while (await stream.ResponseStream.MoveNext())
+                {
+                    Console.WriteLine($"Client received: {stream.ResponseStream.Current.Result}");
+                }
+            });
+
+            for (int i = 0; i < 5; i++)
+            {
+                var request = new GreetEveryoneRequest() { Greeting = greeting };
+                Console.WriteLine($"Client sending: {request}");
+                await stream.RequestStream.WriteAsync(request);
+                await Task.Delay(1000);
+            }
+
+            await stream.RequestStream.CompleteAsync();
+            await readerTask;
+        }
+    }
+}
---
+++ GrpcServer/Services/GreetingServiceImpl.cs
@@ -0,0 +1,58 @@
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class GreetingServiceImpl : GreetingService.GreetingServiceBase
+    {
+        public override Task<GreetingResponse> Greet(GreetingRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new GreetingResponse() { Result = $"Hello {request.Greeting.FirstName} {request.Greeting.LastName}" });
+        }
+
+        public override async Task GreetManyTimes(GreetManyTimesRequest request, IServerStreamWriter<GreetManyTimesResponse> responseStream, ServerCallContext context)
+        {
+            var response = new GreetManyTimesResponse()
+            {
+                Result = $"Hello {request.Greeting.FirstName} {request.Greeting.LastName}"
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                await responseStream.WriteAsync(response);
+                await Task.Delay(1000);
+            }
+        }
+
+        public override async Task<LongGreetResponse> LongGreet(IAsyncStreamReader<LongGreetRequest> requestStream, ServerCallContext context)
+        {
+            var response = new LongGreetResponse();
+            //while (await requestStream.MoveNext())
+            //{
+            //    response.Result += $"Hello {requestStream.Current.Greeting.FirstName} {requestStream.Current.Greeting.LastName}{Environment.NewLine}";
+            //}
+
+            await foreach (var item in requestStream.ReadAllAsync())
+            {
+                response.Result += $"Hello {item.Greeting.FirstName} {item.Greeting.LastName}{Environment.NewLine}";
+            }
+
+            return response;
+        }
+
+        public override async Task GreetEveryone(IAsyncStreamReader<GreetEveryoneRequest> requestStream, IServerStreamWriter<GreetEveryoneResponse> responseStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var response = new GreetEveryoneResponse()
+                {
+                    Result = $"Hello {requestStream.Current.Greeting.FirstName} {requestStream.Current.Greeting.LastName}"
+                };
+
+                await responseStream.WriteAsync(response);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
